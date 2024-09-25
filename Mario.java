fef839e5aef94491462b3a6e7b3bff5432721ac0
Steven <Pospinachico@gmail.com>
2024-09-24 23:12:09 -0500

    Mario

--- Mario.java
+++ Mario.java
@@ -1,43 +1,57 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package javaapplication3;
+package principal;
+
 class Mario {
+
     private String nombre;
     private int vida;
     private int ataque;
     private int defensa;
     private int velocidad;
-public Mario (String nombre, int vida,int ataque, int defensa,int velocidad) {
+    private int posicion;
+
+    public Mario(String nombre, int vida, int ataque, int defensa, int velocidad) {
         this.nombre = nombre;
         this.vida = vida;
         this.ataque = ataque;
-        this.defensa= defensa;
+        this.defensa = defensa;
         this.velocidad = velocidad;
     }
-     
-     public void moverAdelante(){
-System.out.println(nombre + " se ha movido adelante.");
 
-}
-       public void moverAtras() {
-        System.out.println(nombre + " se ha movido Atras.");
+    public void moverAdelante() {
+        System.out.println(nombre + " Mario se ha movido adelante.");
+        posicion++;
+    }
+
+    public void moverAtras() {
+        System.out.println(nombre + "Mario se ha movido Atras.");
+        posicion--;
+    }
+
+    public void Salto() {
+
+        System.out.println(nombre + " Mario ha saltado");
+    }
+
+    public void tocoHongo() {
+        vida = 0;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void vida() {
 
     }
-     public void tocarHongo() {
-        vida = 0; 
-        System.out.println(nombre + " ha tocado un hongo y ha muerto.");
-    }
-    public void vida(){
-        
-    }
-    
-    public void lanzarObjeto(){
-        
-    }
-    
+
+   
+
 }
