a562ea2aa069fc685275157f4232c6f845e90fd2
Steven <Pospinachico@gmail.com>
2024-09-24 23:12:41 -0500

    juego

--- Principal.java
+++ Principal.java
@@ -1,25 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package javaapplication3;
+package principal;
 
 import java.util.Scanner;
-import java.util.Scanner;
 
-/**
- *
- * @author Usuario
- */
 public class Principal {
 
-    static Mario mario;
-    static Hongo hongo;
+    static Scanner lector = new Scanner(System.in);
+    static Mario mario ; 
+    static Hongo hongo ; 
 
-    static void Mario() {
-        int mover = Mario.moverAdelante();
-    }
+    public static void main(String[] args) {
+        char tecla; // Declare tecla variable
 
-    static void hongo() {
+        do {
+            System.out.println("Seleecciona una opción");
+            System.out.println("w. Mario salta ");
+            System.out.println("d. Mario se mueve hacia adelante");
+            System.out.println("a. Mario se mueve hacia atras");
+            System.out.println("z. Termina el juego");
+
+            tecla = lector.next().charAt(0);
+            tecla = Character.toLowerCase(tecla);
+
+            switch (tecla) {
+
+                case 'w':
+                    mario.Salto();
+                    break;
+                case 'd':
+                    mario.moverAdelante();
+                    break;
+                case 'a':
+                    mario.moverAtras();
+                    break;
+                case 'z':
+                    System.out.println("saliendo del juego");
+                    break;
+                default:
+                    System.out.println("Tecla inválida");
+            }
+
+            if (subZero.getVida() <= 0 || scorpio.getVida() <= 0) {
+                String nombreGanador = (subZero.getVida() <= 0) ? scorpio.getNombre() : subZero.getNombre();
+                System.out.println("El juego terminó, y el ganador es " + nombreGanador);
+                tecla = 'z';
+            }
+        } while (tecla != 'z');
     }
 }
+
+
+
+
+
