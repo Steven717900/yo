1a954d84c681cee62c727ae8a7f590d58d10240b
Steven <Pospinachico@gmail.com>
2024-09-24 23:11:21 -0500

    HONGo
    a

--- Hongo.java
+++ Hongo.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package javaapplication3;
+package principal;
 
-/**
- *
- * @author Usuario
- */
-public class Hongo {
-    
+class Hongo {
+
+    private int posicion;
+
+    private String nombre;
+    private int vida;
+    private int ataque;
+    private int defensa;
+    private int velocidad;
+
+    public Hongo(String nombre, int vida, int ataque, int defensa, int velocidad) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.velocidad = velocidad;
+    }
+
+    public void moverAdelante() {
+        System.out.println(nombre + " Hongo se ha movido adelante.");
+        posicion++;
+    }
+
+    public void moverAtras() {
+        System.out.println(nombre + "Hongo se ha movido Atras.");
+        posicion--;
+    }
+
+    public void tocaMario(Mario mario) {
+        mario.tocoHongo();
+    }
+
+    public int getPosicion() {
+        return posicion;
+
+    }
 }
