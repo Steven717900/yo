36f99dd9acee1f6870680ba610bdb10c5fdb2b0d
Steven717900 <pospinachico@gmail.com>
2024-09-26 10:32:58 -0500

    Update Principal.java
    monda

--- Principal.java
+++ Principal.java
@@ -1,4 +1,5 @@
 package principal;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,26 +8,21 @@ public class Principal {
     static Scanner lector = new Scanner(System.in);
     static Mario mario;
     static Hongo hongo;
+    
     static Random random = new Random();
-    static int puntosDeVidaMario = 10;
-    static int puntosDeVidaHongo = 1;
-    static int velocidadMario = 1;
-    static int velocidadHongo = 1;
-    static int tiempoLimite = 60; // se mide en segundos
+    static int tiempoLimite = 30;
     static int tiempoActual = 0;
-    static int puntosMario = 0; // Puntuación de Mario
     static boolean hongoHaAparecido = false;
 
     public static void main(String[] args) {
-        char tecla; 
+        char tecla;
 
-        mario = new Mario(0, velocidadMario, puntosDeVidaMario);
-
+        mario = new Mario(0, 1, 10, 0); 
         do {
-            System.out.println("Seleccione una opción");
+            System.out.println("Seleccione una opcion");
             System.out.println("w. Mario salta ");
             System.out.println("d. Mario se mueve hacia adelante");
-            System.out.println("a. Mario se mueve hacia atrás");
+            System.out.println("a. Mario se mueve hacia atras");
             System.out.println("z. Termina el juego");
             System.out.println("Tiempo restante: " + (tiempoLimite - tiempoActual) + " segundos");
 
@@ -35,34 +31,34 @@ public class Principal {
             double probabilidad = 0.1; //posibilidad del 10
             if (numeroAleatorio < probabilidad && !hongoHaAparecido) {
                 hongoHaAparecido = true;
-                System.out.println("¡Un hongo ha aparecido!");
+                System.out.println("Un hongo ha aparecido");
 
-                hongo = new Hongo(random.nextInt(10), velocidadHongo);
+                hongo = new Hongo(random.nextInt(10), 1, 1);
             }
 
             if (hongoHaAparecido) {
-                System.out.println("¡Un hongo ha aparecido!");
+                System.out.println("Un hongo ha aparecido");
                 System.out.println("Mario solo puede saltar para matarlo");
                 tecla = lector.next().charAt(0);
                 tecla = Character.toLowerCase(tecla);
 
-               //eleccion de marioooo
+                //eleccion de marioooo  :)
                 if (tecla == 'w') {
                     System.out.println("Mario ha saltado y matado al hongo");
-                    puntosDeVidaMario += 10;
-                    puntosMario += 10;
+                    mario.setPuntosDeVida(mario.getPuntosDeVida() + 10);
+                    mario.setPuntos(mario.getPuntos() + 10);
                     hongoHaAparecido = false;
                 } else if (tecla == 'd') {
-                    System.out.println("Mario ha elegido moverse adelante y ha muerto");
+                    System.out.println("Mario ha elegido moverse 5 pasos adelante y ha muerto");
                     System.out.println("Mario ha muerto");
                     break;
                 } else if (tecla == 'a') {
-                    System.out.println("Mario se ha devuelto hacia atrás y el hongo lo sigue");
-                    mario.moverAtras();
+                    System.out.println("Mario se ha devuelto 5 pasos hacia atras y el hongo lo sigue");
+                    mario.moverAtras(5);
                     hongo.moverAtras();
                 } else if (tecla == 'z') {
                     System.out.println("Saliendo del juego...");
-                    System.exit(0); // Terminar el programa
+                    System.exit(0);
                 } else {
                     System.out.println("Tecla inválida");
                 }
@@ -72,37 +68,30 @@ public class Principal {
 
                 switch (tecla) {
                     case 'w':
-                        mario.Salto();
+                        mario.Salto(2);
+                        System.out.println("Mario se ha elevado 2 bloques");
                         break;
                     case 'd':
-                        mario.moverAdelante();
+                        mario.moverAdelante(5);
+                        System.out.println("Mario se movio 5 pasos adelante");
                         break;
                     case 'a':
-                        mario.moverAtras();
+                        mario.moverAtras(5);
+                        System.out.println("Mario se movio 5 pasos atras");
                         break;
                     case 'z':
                         System.out.println("Saliendo del juego...");
-                        System.exit(0); // Terminar el programa
-                        break;
+                        System.exit(0);
                     default:
                         System.out.println("Tecla inválida");
                 }
             }
 
-            // Actualizar el tiempo
             tiempoActual++;
-            if (tiempoActual >= tiempoLimite) {
-                System.out.println("¡El tiempo ha expirado!");
-                System.out.println(":-)"); // Cara de Mario feliz
+            if (tiempoActual > tiempoLimite) {
+                System.out.println("Tiempo agotado. Fin del juego.");
                 break;
             }
-
-            // Mostrar el estado actual del juego
-            System.out.println("Puntos de vida de Mario: " + puntosDeVidaMario);
-            System.out.println("Puntos de vida del hongo: " + puntosDeVidaHongo);
-            System.out.println("Puntuación de Mario: " + puntosMario);
         } while (true);
     }
 }
-
-
