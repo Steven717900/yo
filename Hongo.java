4594b716461c537282dd2ecb4d43c21cbde1d82a
Steven717900 <pospinachico@gmail.com>
2024-09-26 10:33:33 -0500

    Update Hongo.java
    aa

--- Hongo.java
+++ Hongo.java
@@ -1,13 +1,19 @@
+
 package principal;
 
 public class Hongo {
 
     private int posicion;
+    private int vida;
     private int velocidad;
 
-    public Hongo(int posicion, int velocidad) {
+    public Hongo(int posicion, int velocidad, int vida) {
+        if (posicion < 0 || velocidad < 0 || vida < 0) {
+            throw new IllegalArgumentException("Position, velocity and life must be non-negative");
+        }
         this.posicion = posicion;
         this.velocidad = velocidad;
+        this.vida = vida;
     }
 
     public void moverAdelante() {
@@ -20,5 +26,26 @@ public class Hongo {
 
     public int getPosicion() {
         return this.posicion;
+    }
+
+    public int getVida() {
+        return this.vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getVelocidad() {
+        return this.velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Hongo [posicion=" + posicion + ", vida=" + vida + ", velocidad=" + velocidad + "]";
     }
 }
